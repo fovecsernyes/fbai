c58eb142513260099f0d186201c54d7bddbc845a
Márk Vecsernyés <kurtaazz@gmail.com>
2018-08-29 12:46:40 +0200

    added Pipe class

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
-gravity = 1.5;
+gravity = 1.5; //userinput
 
+//bird class
 var Bird = function(src)
 {
     var self = new Image();
@@ -10,23 +11,52 @@ var Bird = function(src)
     self.bY = 150;
     self.src = src;
     self.fitness = 0;
+    self.alive = true;
+
     self.update = function()
     {
         self.bY += gravity;
     }
+
     self.moveUp = function()
     {
         self.bY -= 25;
+    }
+
+    self.kill = function()
+    {
+        self.alive = false;
+    }
+
+    return self;
+}
+
+var Pipe = function(src, gap)
+{
+    var self = new Image();
+    self.src = src;
+    self.pX = cvs.width;
+    self.pY = gap;
+
+    self.update = function()
+    {
+        self.pX--;
     }
     return self;
 }
 
 var bird = new Bird("images/bird.png"); //bird
+var pipeNorth = new Pipe("images/pipeNorth.png", 0);
+var pipeSouth = new Pipe("images/pipeSouth.png", 400);
 
 function draw() //game
 {
-    ctx.drawImage(bird,bird.bX,bird.bY);
+    ctx.drawImage(bird, bird.bX, bird.bY);
+    ctx.drawImage(pipeNorth, pipeNorth.pX, pipeNorth.pY);
+    ctx.drawImage(pipeSouth, pipeSouth.pX, pipeSouth.pY);
     bird.update();
+    pipeNorth.update();
+    pipeSouth.update();
     requestAnimationFrame(draw); 
 }
 
