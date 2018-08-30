61fd29bcc47053a1cb3f433d36c9c0fb129e4112
Márk Vecsernyés <kurtaazz@gmail.com>
2018-08-30 12:03:51 +0200

    autoformatting

--- game.js
+++ game.js
@@ -5,14 +5,13 @@ var gravity = 1.5; //userinput
 var population = 150; //userinput
 var gap = 85; //user input
 
-var LoadImages = function()
-{
+var LoadImages = function () {
     bg = new Image();
     fg = new Image();
-    
+
     bg.src = "images/bg.png";
     fg.src = "images/fg.png";
-    
+
     bird = new Image();
     pipeNorth = new Image();
     pipeSouth = new Image();
@@ -23,10 +22,8 @@ var LoadImages = function()
     return self;
 }
 
-var Bird = function()
-{
-    var self = 
-    {
+var Bird = function () {
+    var self = {
         bX: 10,
         bY: 150,
         //angle: 0,
@@ -34,44 +31,38 @@ var Bird = function()
         alive: true
     }
 
-    self.update = function()
-    {
+    self.update = function () {
         self.bY += gravity;
-        if(Math.floor(18 * Math.random()) % 18 === 0){
+        if (Math.floor(18 * Math.random()) % 18 === 0) {
             self.moveUp();
         }
     }
 
-    self.moveUp = function()
-    {
+    self.moveUp = function () {
         self.bY -= 25;
     }
 
-    self.kill = function()
-    {
+    self.kill = function () {
         self.alive = false;
     }
 
     return self;
 }
 
-var Pipe = function(y)
-{
-    var self = 
+var Pipe = function (y) {
+    var self =
     {
         pX: cvs.width,
         pY: y
     }
 
-    self.update = function()
-    {
+    self.update = function () {
         self.pX--;
     }
     return self;
 }
 
-var Game = function()
-{
+var Game = function () {
     var img = LoadImages();
 
     var pipe = [];
@@ -79,58 +70,48 @@ var Game = function()
     alive = population;
 
     var bird = [];
-    for (var i = 0; i < population; i++ )
-    {
+    for (var i = 0; i < population; i++) {
         bird[i] = Bird();
-    }   
+    }
 
-    self.update = function()
-    {
-        ctx.drawImage(img.bg,0,0);
-        for(var i = 0; i < pipe.length; i++)
-        {
-            if(pipe[i].pX <  0 - img.pipeNorth.width)
-            {
+    self.update = function () {
+        ctx.drawImage(img.bg, 0, 0);
+        for (var i = 0; i < pipe.length; i++) {
+            if (pipe[i].pX < 0 - img.pipeNorth.width) {
                 pipe.shift();
             }
 
-            constant = img.pipeNorth.height+gap;
+            constant = img.pipeNorth.height + gap;
             ctx.drawImage(img.pipeNorth, pipe[i].pX, pipe[i].pY);
-            ctx.drawImage(img.pipeSouth, pipe[i].pX, pipe[i].pY+constant);
+            ctx.drawImage(img.pipeSouth, pipe[i].pX, pipe[i].pY + constant);
             pipe[i].update();
 
-            if( pipe[i].pX == 125 )
-            {
-                pipe.push(Pipe(Math.floor(Math.random()*img.pipeNorth.height)-img.pipeNorth.height)); 
+            if (pipe[i].pX == 125) {
+                pipe.push(Pipe(Math.floor(Math.random() * img.pipeNorth.height) - img.pipeNorth.height));
             }
 
         }
 
-        ctx.drawImage(img.fg,0,cvs.height - img.fg.height);
+        ctx.drawImage(img.fg, 0, cvs.height - img.fg.height);
 
-        for (var i = 0; i < bird.length; i++)
-        {
-            if (bird[i].alive)
-            {
+        for (var i = 0; i < bird.length; i++) {
+            if (bird[i].alive) {
                 ctx.drawImage(img.bird, bird[i].bX, bird[i].bY);
                 bird[i].update();
-                for (var j = 0; j < pipe.length; j++)
-                {
-                    if( bird[i].bX + img.bird.width >= pipe[j].pX && bird[i].bX <= pipe[j].pX + img.pipeNorth.width && (bird[i].bY <= pipe[j].pY + img.pipeNorth.height || bird[i].bY+img.bird.height >= pipe[j].pY+constant) || bird[i].bY + img.bird.height >=  cvs.height - fg.height)
-                    {
+                for (var j = 0; j < pipe.length; j++) {
+                    if (bird[i].bX + img.bird.width >= pipe[j].pX && bird[i].bX <= pipe[j].pX + img.pipeNorth.width && (bird[i].bY <= pipe[j].pY + img.pipeNorth.height || bird[i].bY + img.bird.height >= pipe[j].pY + constant) || bird[i].bY + img.bird.height >= cvs.height - fg.height) {
                         bird[i].kill();
                         alive--;
-                    } 
+                    }
                 }
             }
         }
         //sometimes goes under 0 (tried with #180 birds)
-        if (alive == 0)
-        {
+        if (alive == 0) {
             location.reload();
         }
 
-        requestAnimationFrame(update); 
+        requestAnimationFrame(update);
     }
     return self;
 }
