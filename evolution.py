9da0aefb06356a47b564af5777ef26e8525cefeb
Márk Vecsernyés <kurtaazz@gmail.com>
2018-09-13 00:38:51 +0200

    added flask

--- evolution.py
+++ evolution.py
@@ -1,2 +1,20 @@
-f = open("demofile.txt", "a")
-f.write("Now the file has one more line!")
+from flask import Flask, render_template, request
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/', methods=['GET','POST'])
+def dropdown():
+    gravity = [x/10 for x in range(10, 21)]
+    population = [5*x for x in range(1, 11)]
+    gap = [5*x for x in range(14, 25)]
+
+    if request.method == 'GET':
+         print("Gravity: " + request.args.get('gravity', "None") )
+         print("Population: " + request.args.get('population', "None") )
+         print("Gap: " + request.args.get('gap', "None") )
+
+    return render_template('index.html', gravity=gravity, population=population, gap=gap)
+
+if __name__ == "__main__":
+    app.run()
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#this script only works on MacOS
-python evolution.py
-echo AI client started!
-
-open -a "Google Chrome" "index.html"
-echo Game started
-
