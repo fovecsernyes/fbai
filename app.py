f7f8968566a3fa07b169987b86aa5de935ff7a21
Márk Vecsernyés <kurtaazz@gmail.com>
2018-09-18 23:14:09 +0200

    M: BE: modified app routes

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from database import Database
 from flask import *
-import gameservice
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route('/', methods=['GET','POST'])
-def HandleServicesRequest():
-    return gameservice.handle_command(request.form)
+@app.route('/', methods=['GET'])
+def GetRequest():
+    database_status=""
+    return render_template('index.html', database_status=database_status)
+
+@app.route('/', methods=['POST'])
+def ApplyRequest():
+    print(request.form)
+    database_status = database.create_tables()
+    return render_template('index.html', database_status=database_status)
+
+@app.route('/start', methods=['POST'])
+def StartRequest():
+    print(request.form)
+    status = "ack"
+    return render_template('index.html', status=status) #not working
+    
+
 
 if __name__ == "__main__":
+    database = Database()
     app.run()
