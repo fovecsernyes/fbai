4b6dc390b5b46b20bcfbed0b14736d7036926971
Márk Vecsernyés <kurtaazz@gmail.com>
2018-09-18 20:05:15 +0200

    A: BE: added post seperator

--- gameservice.py
+++ gameservice.py
@@ -3,14 +3,28 @@ from database import Database
 
 database = Database()
 
+def NoKey():
+    database_status=""
+    return render_template('index.html', database_status=database_status)
+
+def ApplyCommand():
+    database_status = database.create_tables()
+    return render_template('index.html', database_status=database_status)
+
+def StartRoundCommand():
+    status="ACK"
+    return render_template('index.html', status=status)
+    
+
 
 def handle_command(flask_request: str) -> str:
 
-    print(flask_request)
 
+    command = str(flask_request.get("command"))
 
-    database_status = ""
-    #database_status = database.create_tables()
-        
+    commands = {"None": NoKey,
+                "Apply" : ApplyCommand,
+                "StartRound" : StartRoundCommand,
+    }
 
-    return render_template('index.html', database_status=database_status)
+    return commands[command]()    
